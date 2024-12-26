32a8f02d834df6e31fec8a4c97cf5a5297849cb7
Narges <nargesnovhekhan@gmail.com>
2024-12-26 20:57:06 +0330

    Add deleting a task

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.querySelector('#push').onclick = function () {
     if (document.querySelector('#new-task input').value.length == 0) {
         alert("Please Enter a Task")
-    }
-    else {
+    } else {
         document.querySelector('#tasks').innerHTML += `
          <div class= "task">
                 <span id="taskname">${document.querySelector('#new-task input').value}</span>
+                <button class="delete">
+                 <i class="fa-solid fa-trash fa-flip-horizontal fa-lg"></i>
+                </button>
+         </div>
         `;
     }
 }
