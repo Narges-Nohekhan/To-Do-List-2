105801fe88e77417b465ca12db30b576ef22a5d8
Narges <nargesnovhekhan@gmail.com>
2024-12-29 01:08:07 +0330

    delete tasks and cross out a completed task.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('#push').onclick = function () {
-    if (document.querySelector('#new-task input').value.length == 0) {
+    if (document.querySelector('#new-task input').value.length === 0) {
         alert("Please Enter a Task")
     } else {
         document.querySelector('#tasks').innerHTML += `
@@ -10,5 +10,19 @@ document.querySelector('#push').onclick = function () {
                 </button>
          </div>
         `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+
+        }
+        var tasks = document.querySelectorAll(".task");
+        for (var i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function () {
+                this.classList.toggle('completed');
+            }
+
+        }
     }
 }
